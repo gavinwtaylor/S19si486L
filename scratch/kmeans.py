099264fb10a878997e8001d9ad1aa9b52d50fb78
Gavin Taylor <taylor@usna.edu>
2018-12-18 14:09:54 -0500

    kmeans demo

--- scratch/kmeans.py
+++ scratch/kmeans.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python3
-import idx2numpy
+#import idx2numpy
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
+from math import sqrt
+import h5py
+
+def drawIt(data,centroids,assign):
+  for row in range(len(data)):
+    plt.plot(data[row,0],data[row,1],'.',color='C'+str(int(assign[row])))
+  for c in centroids:
+    plt.plot(c[0],c[1],'o')
+  plt.show()
 
 def kmeans(data,k):
   (n,m)=data.shape
   centroids=data[np.random.permutation(n)[:k],:]
   run=True
   count=0
-  assign=np.zeros(n)
+  assign=np.zeros(n,dtype=int)
   while run:
     print(centroids)
     run=False
@@ -25,13 +35,26 @@ def kmeans(data,k):
       if minind!=assign[i]:
         assign[i]=minind
         run=True
+    drawIt(data,centroids,assign)
     for c in range(k):
       centroids[c,:]=np.mean(data[np.where(assign==c)[0],:],0)
-      print(c,np.where(assign==c)[0].shape)
+  totalDist=0
+  for i in range(n):
+    totalDist=totalDist+sqrt(np.linalg.norm(data[i,:]-\
+        centroids[assign[i],:]))
+  return (centroids,totalDist/n,assign)
 
-pics=idx2numpy.convert_from_file('../data/mnist/train-images-idx3-ubyte')
-shaped=np.reshape(pics,(60000,784))
-#shaped=np.random.multivariate_normal(np.array([2,2]),np.array([[1,0],[0,1]]),size=5)
-#shaped=np.concatenate((shaped,np.random.multivariate_normal(np.array([-2,-2]),np.array([[1,0],[0,1]]),size=7)))
-#print(shaped)
-kmeans(shaped,2)
+#pics=idx2numpy.convert_from_file('../data/mnist/train-images-idx3-ubyte')
+#shaped=np.reshape(pics,(60000,784))
+
+parser=argparse.ArgumentParser()
+parser.add_argument("filename")
+parser.add_argument("k",type=int)
+args=parser.parse_args()
+filename=args.filename
+
+with h5py.File(filename,'r') as f:
+  data=f['data'][:]
+
+centroids,avgDist,assign=kmeans(data,args.k)
+drawIt(data,centroids,assign)
