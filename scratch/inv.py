141396322e5ded5883239c925f5aae146390128f
Gavin Taylor <taylor@usna.edu>
2019-01-03 10:46:41 -0500

    moved things over to instructor repo

--- scratch/inv.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-import numpy as np
-
-a=np.array([[2,3,4],[3,4,5],[4,5,6]])
-b=np.array([[1,0,1,0],[0,1,1,0],[1,1,0,1],[1,1,1,0]])
-print(np.linalg.inv(b))
-print(np.linalg.inv(a))
