8fe2cc549dd833c5ebafc63fca169ef85307ac3d
Gavin Taylor <taylor@usna.edu>
2019-01-22 13:13:35 -0500

    svd final

--- class/svd/mnistSVDDemo.py
+++ class/svd/mnistSVDDemo.py
@@ -28,6 +28,7 @@ def showReconstruct(data,rebuild):
   plt.subplot(3,2,6)
   plt.imshow(rebuild[2,:,:],cmap='gray')
   plt.show()
+
 parser=argparse.ArgumentParser()
 parser.add_argument("mnistDirectory")
 parser.add_argument("--keepK",type=int)
@@ -52,6 +53,7 @@ print(shapenThrees.shape)
 ############
 
 u,s,vh=np.linalg.svd(shapenThrees)
+print(u.shape,Sigma.shape,vh.shape)
 
 plt.plot(s)
 #plt.ylim((-5,100))
@@ -63,7 +65,6 @@ keepK=args.keepK
 s[keepK:]=np.zeros(784-keepK)
 Sigma=np.zeros((numThrees,784))
 Sigma[0:784,0:784]=np.diag(s)
-print(u.shape,Sigma.shape,vh.shape)
 
 shapenThrees=u@Sigma@vh
 newThrees=np.reshape(shapenThrees,(numThrees,28,28))
