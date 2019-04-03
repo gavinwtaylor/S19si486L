20f84c11628dfd60ab7e21c34265235ec8fed088
Gavin Taylor <taylor@usna.edu>
2019-04-03 15:03:22 -0400

    tribbleex

--- class/nn/tribbleEx.py
+++ class/nn/tribbleEx.py
@@ -10,6 +10,7 @@ assert gpu>=0 and gpu<4
 os.environ["CUDA_VISIBLE_DEVICES"]=str(gpu)
 
 import tensorflow as tf
+import tensorflow.keras as keras
 from tensorflow.keras import layers
 import numpy as np
 
@@ -27,4 +28,16 @@ model.compile(optimizer=tf.train.AdamOptimizer(0.001),
 data = np.random.random((1000, 32))
 labels = np.random.random((1000, 10))
 
-model.fit(data, labels, epochs=1000, batch_size=32)
+class AccuracyHistory(keras.callbacks.Callback):
+  def on_train_begin(self, logs={}):
+    self.loss = []
+    self.val_loss = []
+
+  def on_epoch_end(self, batch, logs={}):
+    self.loss.append(logs.get('loss'))
+    self.val_loss.append(logs.get('val_loss'))
+
+history=AccuracyHistory()
+model.fit(data, labels, epochs=10, batch_size=32, validation_split=.2, callbacks=[history])
+
+print(history.loss)
